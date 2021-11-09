c88dd21ed1f588534a28843787198cf8f7e68c87
sabbirirfan <sabbirirfan29@gmail.com>
2021-11-09 23:30:07 +0600

    tmp_html_updated

---
+++ client/src/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    color: 'rgba(0,183,255, 1)',
+  },
+  image: {
+    height: '60px',
+    width: '60px',
+    marginLeft: '15px',
+  },
+}));
