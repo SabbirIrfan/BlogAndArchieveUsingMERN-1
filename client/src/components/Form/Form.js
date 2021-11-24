5ab4a696fbe00b7e509f1fb71556211a20b731cc
sabbirirfan <sabbirirfan29@gmail.com>
2021-11-24 15:35:08 +0600

    LIKE_DELETE without authentication

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,51 +1,55 @@
-import React, {useState} from 'react';
-import useStyles from './styles';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+
+import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
-const Form = (currentId, setCurrentId) =>{
-    const [postData, setPostData] = useState({
-        creator: '', title: '', message: '', tags: '', selectedFile: ''});
-    const classes = useStyles();
-    const dispatch = useDispatch();
 
-    const clear = () =>{
+const Form = ({ currentId, setCurrentId }) => {
+  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));// getiing the post to update
+  const dispatch = useDispatch();
+  const classes = useStyles();
 
-    };
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault();
+    //populating the form with post data to update
+  useEffect(() => {
+    if (post) setPostData(post); // setting the new data to which got from the form const[postData,setPostData]
+  }, [post]);
+  // clears the post form to it's default state
+  const clear = () => {
+    setCurrentId(0);
+    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  };
 
-        if (currentId) {
-            dispatch(updatePost(currentId, postData));
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        dispatch(createPost(postData));
+    if (currentId === 0) {
+      dispatch(createPost(postData));
+      clear();  // after submitting(clicking the submit button) the edit/update we need to cleare the form
 
-    };
+    } else {
+      dispatch(updatePost(currentId, postData));
+      clear();  // after submitting(clicking the submit button) the edit/update we need to cleare the form
 
-    return (
-        <Paper className={classes.paper}>
-            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit} encType="multipart/form-data" >
-                <Typography variant="h6"  >Creating a Memory</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData,creator: e.target.value})}></TextField>
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData,title: e.target.value})}></TextField>
-                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData,message: e.target.value})}></TextField>
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData,tags: e.target.value})}></TextField>
-                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
-                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth >Submit</Button>
-                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth >Clear</Button>
+    }
+  };
 
-            </form>
-            
-        </Paper>
-    );
-}
+  return (
+    <Paper className={classes.paper}>
+      <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+        <Typography variant="h6">{currentId ? `Updating "${post.title}"` : 'Share Your Thought and Experience'}</Typography>
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+        <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+        <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+        <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+        <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+        <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+      </form>
+    </Paper>
+  );
+};
 
 export default Form;
-
-// <div className={classes.fileInput}>
-//   <FileBase type="file" multiple={false} onDone={(base64) => setPostData({...postData, selectedFile: base64})} ></FileBase>
-//</div>
-                
--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: '30px 0',
@@ -17,4 +17,9 @@ export default makeStyles(() => ({
     width: '60px',
     marginLeft: '15px',
   },
+  // [theme.breakponts.down('sm')]:{
+  //   mainContainer:{
+  //     flexDirection:"column-reverse"
+  //   }
+  // }
 }));
