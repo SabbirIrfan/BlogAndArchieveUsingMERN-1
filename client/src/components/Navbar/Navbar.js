edd87569b2fa36305ffb606f179b2fe32aeb787d
Naimul Hasan <mohammadfahimshah@gmail.com>
2021-11-25 19:31:08 +0600

    added login and sigup form and button.

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {AppBar, Typography} from '@material-ui/core';
+import useStyles from './styles';
+import {Link} from "react-router-dom"
+import { Avatar, Button, Toolbar } from "@mui/material";
+const Navbar = () => {
+    const classes = useStyles();
+    const user = null;
+    return(
+        <AppBar className={classes.appBar} position="static" color="inherit">
+            <div>
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Blend</Typography>
+            </div>
+            <Toolbar className={classes.toolbar}>
+                {user ? (
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{ user.result.name.charAt(0)}</Avatar>
+                        <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+                        <Button variant="contained" className={classes.logout}></Button>
+                    </div>      
+                ) : (
+                    <Button component={Link} to="/auth" variant="contained" color ="primary">Sign In</Button>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
