c0a22965812d0eaea8ee18e80483bb35c29e8689
sabbirirfan <sabbirirfan29@gmail.com>
2021-12-04 04:35:50 +0600

    Revert "ui change"
    This reverts commit 66780dbb8fe7c18aa9b5847d29863abebd789498.

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
           <Route path="/posts" element={<Home userstate={userstate}/>} />
           <Route path="/posts/search" element={<Home userstate={userstate}/>} />
           <Route path="/posts/:id" element = {<PostDetails/>} />
-          <Route path="/auth" element={<Auth setUserId={setUserId}/> }/>
+          <Route path="/auth" element={ (!user ? <Auth setUserId={setUserId}/> : <Navigate to='/posts'/>)}/>
         </Routes>
       </Container>
     </BrowserRouter>
