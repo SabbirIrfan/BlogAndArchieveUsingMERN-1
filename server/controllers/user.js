73bc20153b205cafb4f130656824aa889a161c86
sabbir <sabbirirfan29@gmail.com>
2023-08-29 11:59:05 +0600

    jwt_token_secret update

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -15,7 +15,12 @@ export const signin = async (req, res) => {
         const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
         if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credintials.' });
 
-        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: '1h' })
+        const userObject = { 
+            email: existingUser.email,
+            id: existingUser._id 
+        };
+
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, { expiresIn: '1h' })
         
         res.status(200).json({ result: existingUser, token });
     } catch (error) {
@@ -37,7 +42,12 @@ export const signup = async (req, res) => {
         
         const hashedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName} `, imageUrl, googleId, institude, social, imageData,linkedIn,github })
-        const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: '1h' })
+
+        const userObject = {
+             email: result.email,
+             id: result._id 
+            };
+        const token = jwt.sign(userObject, process.env.JWT_SECRET , { expiresIn: '1h' })
         res.status(200).json({ result, token });
     } catch (error) {
          res.status(500).json({message: "Something went wrong."})
